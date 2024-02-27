4786c1ba3aa56c4e60961b9729ad166e736d81b8
AbbasKothari1552 <ask50405808@gmail.com>
2024-02-27 07:56:17 +0530

    Changes in index.html

--- Trail.h
+++ Trail.h
@@ -1,10 +1,1 @@
-int riddle(int n)
-{
-    int result = 1, i;
- 
-    // loop from 2 to n to get the factorial
-    for (i = 2; i <= n-1; i++) {
-        result *= i;
-    }
-    return result;
-}
+vwrjhvihbibv
